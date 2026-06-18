2ca10a16a765416938da7f0e5e0f4de5af96b163
zescabedo <zescabedo@gmail.com>
2026-06-18 10:49:13 -0500

    update to robot file

--- industry-verticals/retail/src/pages/api/robots.ts
+++ industry-verticals/retail/src/pages/api/robots.ts
@@ -1,6 +1,15 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
 import { RobotsMiddleware } from '@sitecore-content-sdk/nextjs/middleware';
 import scClient from 'lib/sitecore-client';
 import sites from '.sitecore/sites.json';
+
+const SCRUNCH_CRAWLER_RULES = `User-agent: Scrunchbot
+Allow: /
+
+User-agent: ScrunchAI-testbot
+Allow: /`;
+
+const baseHandler = new RobotsMiddleware(scClient, sites).getHandler();
 
 /**
  * API route for serving robots.txt
@@ -9,8 +18,15 @@ import sites from '.sitecore/sites.json';
  * based on the resolved site name. It is commonly
  * used by search engine crawlers to determine crawl and indexing rules.
  */
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const originalSend = res.send.bind(res);
 
-// Wire up the RobotsMiddleware handler
-const handler = new RobotsMiddleware(scClient, sites).getHandler();
+  res.send = (body) => {
+    if (typeof body === 'string' && (res.statusCode === 200 || res.statusCode === 404)) {
+      body = `${body.trimEnd()}\n\n${SCRUNCH_CRAWLER_RULES}`;
+    }
+    return originalSend(body);
+  };
 
-export default handler;
+  return baseHandler(req, res);
+}
